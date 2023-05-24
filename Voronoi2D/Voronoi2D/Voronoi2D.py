b67a8faa50b6c2bb1c5232e5334d26391aaf832d
zubeyir-bodur <zubeyir.bodur@ug.bilkent.edu.tr>
2023-05-24 18:47:11 +0300

    Base GUI impl.

--- Voronoi2D/Voronoi2D/Voronoi2D.py
+++ Voronoi2D/Voronoi2D/Voronoi2D.py
@@ -17,44 +17,44 @@ zoom = 2
 
 
 def mesh_inc(surface, numSeeds):
-    radius = 730
-    seeds = radius * np.random.random((numSeeds, 2)) + 5
+    radius = SCREEN_HEIGHT
+    seeds = (radius + 240, radius - 10) * np.random.random((numSeeds, 2)) + 5
     center = np.mean(seeds, axis=0)
     dt = Delaunay2D(center, 50 * radius)
     print("Adding points...")
     print(seeds)
     for s in seeds:
         dt.addPoint(s)
-        pygame.draw.circle(surface, "#CC00CC", s, 5)
+        pygame.draw.circle(surface, "#FF0000", s, 7)
     asd = dt.generateVoronoi()
     for t in dt.exportTriangles()[0]:
         print(t)
-        pygame.draw.polygon(surface=surface, color="Green", points=[seeds[t[0]], seeds[t[1]], seeds[t[2]]], width=1)
+        pygame.draw.polygon(surface=surface, color=(181, 230, 29), points=[seeds[t[0]], seeds[t[1]], seeds[t[2]]], width=2)
 
 
 def add_inc(surface):
-    surface.fill((160, 160, 160))
-    s = (random.randint(5, 735), random.randint(5, 735))
+    surface.fill((76, 98, 122))
+    s = (random.randint(5, SCREEN_HEIGHT + 245), random.randint(5, SCREEN_HEIGHT-5))
     seeds.append(s)
     center = np.mean(seeds, axis=0)
     dt = Delaunay2D(center, 50 * radius)
     print(seeds)
     for s in seeds:
         dt.addPoint(s)
-        pygame.draw.circle(surface, "#CC00CC", s, 5)
+        pygame.draw.circle(surface, "#FF0000", s, 7)
 
     for t in dt.exportTriangles()[0]:
         print(t)
-        pygame.draw.polygon(surface=surface, color="Green", points=[seeds[t[0]], seeds[t[1]], seeds[t[2]]], width=1)
+        pygame.draw.polygon(surface=surface, color=(181, 230, 29), points=[seeds[t[0]], seeds[t[1]], seeds[t[2]]], width=2)
 
 
 def clear(surface):
-    surface.fill((160, 160, 160))
+    surface.fill((76, 98, 122))
     global seeds
     seeds = []
 
 def get_font(size):  # Returns Press-Start-2P in the desired size
-    return pygame.font.Font("assets/Roboto-Regular.ttf", size)
+    return pygame.font.Font("assets/Karla-Regular.ttf", size)
 
 
 def blitRotateCenter(surf, image, topleft, angle):
@@ -63,8 +63,8 @@ def blitRotateCenter(surf, image, topleft, angle):
     surf.blit(rotated_image, new_rect)
 
 
-NOP_Info = get_font(16).render("No of Points:", True, "Black")
-NOP_RECT = pygame.Rect(1150, 260, 140, 32)
+NOP_Info = get_font(30).render("N: ", True, "Black")
+NOP_RECT = pygame.Rect(1150, 310, 140, 30)
 radius = 730
 center = (380, 380)
 seeds = []
@@ -74,17 +74,17 @@ def inc_screen():
     pygame.display.set_caption("Incremental Algorithm")
 
     fake_screen = screen.copy()
-    pic = pygame.surface.Surface((760, 760))
-    pic.fill((160, 160, 160))
-    zoom_size = (round(760 / zoom), round(760 / zoom))
+    pic = pygame.surface.Surface((SCREEN_HEIGHT + 250, SCREEN_HEIGHT))
+    pic.fill((76, 98, 122))
+    zoom_size = (round(SCREEN_HEIGHT / zoom), round(SCREEN_HEIGHT / zoom))
 
     no_of_points = ""
     active = False
     zoomActive = False
-
+    consecutive_nexts = 0
     while True:
         mouse = pygame.mouse.get_pos()
-        fake_screen.fill((160, 160, 160))
+        fake_screen.fill((161, 200, 207))
         fake_screen.blit(pic, (1, 1))
         screen.blit(pygame.transform.scale(fake_screen, screen.get_rect().size), (0, 0))
         if zoomActive:
@@ -92,25 +92,25 @@ def inc_screen():
             zoom_area.center = (mouse[0], mouse[1])
             zoom_surf = pygame.Surface(zoom_area.size)
             zoom_surf.blit(screen, (0, 0), zoom_area)
-            zoom_surf = pygame.transform.scale(zoom_surf, (760, 760))
+            zoom_surf = pygame.transform.scale(zoom_surf, (SCREEN_HEIGHT + 251, SCREEN_HEIGHT))
             screen.blit(zoom_surf, (0, 0))
 
-        NOP_TEXT = get_font(16).render(no_of_points, True, "Black")
+        NOP_TEXT = get_font(25).render(no_of_points, True, "Black")
         pygame.draw.rect(screen, "Black", NOP_RECT, 2)
-        screen.blit(NOP_Info, (NOP_RECT.x - 225, NOP_RECT.y + 5))
-        screen.blit(NOP_TEXT, (NOP_RECT.x + 5, NOP_RECT.y + 5))
+        screen.blit(NOP_Info, (NOP_RECT.x - 50, NOP_RECT.y - 5))
+        screen.blit(NOP_TEXT, (NOP_RECT.x + 5, NOP_RECT.y))
         NOP_RECT.w = max(100, NOP_TEXT.get_width() + 10)
 
-        MESH = Button(image=None, pos=(1020, 340), text_input="MESH",
-                      font=get_font(30), base_color="White", hovering_color="Green")
-        STEP = Button(image=None, pos=(1080, 420), text_input="STEP-BY-STEP",
-                      font=get_font(30), base_color="White", hovering_color="Green")
-        CLEAR = Button(image=None, pos=(1190, 340), text_input="CLEAR",
-                      font=get_font(30), base_color="White", hovering_color="Green")
-        BACK = Button(image=None, pos=(1175, 700), text_input="BACK",
-                      font=get_font(30), base_color="White", hovering_color="Green")
+        GENERATE = Button(image=None, pos=(1150, 370), text_input="Generate",
+                      font=get_font(25), base_color="Black", hovering_color="Yellow")
+        STEP = Button(image=None, pos=(1155, 420), text_input="Next Step",
+                      font=get_font(25), base_color="Black", hovering_color="Yellow")
+        CLEAR = Button(image=None, pos=(1148, 470), text_input="Clear All",
+                      font=get_font(25), base_color="Black", hovering_color="Yellow")
+        BACK = Button(image=None, pos=(1150, 725), text_input="Back to Main Menu",
+                      font=get_font(25), base_color="Black", hovering_color="Yellow")
 
-        for button in [MESH, STEP, CLEAR, BACK]:
+        for button in [GENERATE, STEP, CLEAR, BACK]:
             button.changeColor(mouse)
             button.update(screen)
 
@@ -120,19 +120,26 @@ def inc_screen():
                 sys.exit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if BACK.checkForInput(mouse):
+                    consecutive_nexts = 0
                     main_menu()
-                elif MESH.checkForInput(mouse):
+                elif GENERATE.checkForInput(mouse):
                     no = int(no_of_points)
+                    consecutive_nexts = 0
+                    clear(pic)
                     mesh_inc(pic, no)
                 elif CLEAR.checkForInput(mouse):
+                    consecutive_nexts = 0
                     clear(pic)
                 elif STEP.checkForInput(mouse):
+                    if consecutive_nexts == 0:
+                        clear(pic)
+                    consecutive_nexts += 1
                     add_inc(pic)
                 if NOP_RECT.collidepoint(event.pos):
                     active = True
                 else:
                     active = False
-                if mouse <= (760, 760):
+                if mouse <= (SCREEN_HEIGHT + 251, SCREEN_HEIGHT):
                     zoomActive = not zoomActive
 
             elif event.type == pygame.KEYDOWN and active:
@@ -146,19 +153,30 @@ def inc_screen():
 
 
 def main_menu():
-    pygame.display.set_caption("Delaunay Triangulation Main Menu")
-    screen.fill((160, 160, 160))
+    pygame.display.set_caption("CS 478 Project - Implementing Three Voronoi Diagram Computation Algorithms " 
+                               +  "and Comparing Their Performance")
+    screen.fill((161, 200, 207))
     while True:
         mouse = pygame.mouse.get_pos()
 
-        menu_text = get_font(100).render("Main Menu", True, "#CC00CC")
-        menu_rect = menu_text.get_rect(center=(640, 100))
+        menu_text = get_font(30).render("Implementation of Voronoi Diagram", True, (126, 47, 40))
+        menu_rect = menu_text.get_rect(center=(275, 100))
+        screen.blit(menu_text, menu_rect)
+        menu_text = get_font(30).render("Algorithms in Python", True, (126, 47, 40))
+        menu_rect = menu_text.get_rect(center=(275, 130))
         screen.blit(menu_text, menu_rect)
 
-        INC_BUTTON = Button(image=None, pos=(640, 350), text_input="Incremental Algorithm",
-                            font=get_font(40), base_color="White", hovering_color="Green")
+        INC_BUTTON = Button(image=None, pos=(200, 250), text_input="Randomized Incremental",
+                            font=get_font(25), base_color="Black", hovering_color="Yellow")
 
-        for button in [INC_BUTTON]:
+        FORTUNE_BUTTON = Button(image=None, pos=(165, 300), text_input="Fortune's Algorithm",
+                            font=get_font(25), base_color="Black", hovering_color="Yellow")
+        FLIP_BUTTON = Button(image=None, pos=(160, 350), text_input="Flipping Algorithm",
+                            font=get_font(25), base_color="Black", hovering_color="Yellow")
+        QUIT = Button(image=None, pos=(75, 400), text_input="Exit",
+                            font=get_font(25), base_color="Black", hovering_color="Yellow")
+
+        for button in [INC_BUTTON, FORTUNE_BUTTON, FLIP_BUTTON, QUIT]:
             button.changeColor(mouse)
             button.update(screen)
 
@@ -169,6 +187,9 @@ def main_menu():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if INC_BUTTON.checkForInput(mouse):
                     inc_screen()
+                elif QUIT.checkForInput(mouse):
+                    pygame.quit()
+                    sys.exit()
 
         pygame.display.update()
 
