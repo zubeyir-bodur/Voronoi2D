ef57364d3fce338cd053834eb50de803c9a2c37c
zubeyir-bodur <zubeyir.bodur@ug.bilkent.edu.tr>
2023-05-29 16:20:43 +0300

    gaussian & uniform distribution

--- Voronoi2D/Voronoi2D/Button.py
+++ Voronoi2D/Voronoi2D/Button.py
@@ -12,20 +12,30 @@ class Button:
         self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
         self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
 
-    def update(self, screen):
+    def draw(self, screen):
         if self.image is not None:
             screen.blit(self.image, self.rect)
         screen.blit(self.text, self.text_rect)
 
-    def checkForInput(self, position):
+    def mousePressEvent(self, position):
         if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top,
                                                                                           self.rect.bottom):
             return True
         return False
 
-    def changeColor(self, position):
+    def mouseMoveEvent(self, position):
         if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top,
                                                                                           self.rect.bottom):
             self.text = self.font.render(self.text_input, True, self.hovering_color)
         else:
             self.text = self.font.render(self.text_input, True, self.base_color)
+
+
+    def setText(self, textInput, position=(0, 0)):
+        cur_color = None
+        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top,
+                                                                                          self.rect.bottom):
+            cur_color = self.hovering_color
+        else:
+            cur_color = self.base_color
+        self.font.render(self.text_input, True, cur_color)
--- Voronoi2D/Voronoi2D/main.py
+++ Voronoi2D/Voronoi2D/main.py
@@ -1,3 +1,4 @@
+from multiprocessing.dummy import current_process
 import sys
 import pygame
 import numpy as np
@@ -31,16 +32,43 @@ radius = SCREEN_HEIGHT / 2
 center = ((SCREEN_HEIGHT + 250) / 2, SCREEN_HEIGHT / 2)
 points = []
 incremental_dt = Voronoi2DIncremental(center, 50 * radius)
+distribution_texts = {
+        0: "Random", 
+        1: "Gaussian", 
+        2: "Uniform"
+    }
+current_distribution = 0
 
 
-def get_random_point(center_):
-    random_numbers_btw_zero_and_two_pi = np.random.random(size=1)*2*math.pi
-    random_numbers_btw_zero_and_radius_min_spacing = np.random.random(size=1)
-    theta__ = clamp(random_numbers_btw_zero_and_two_pi[0], 0, 2*math.pi)
-    radius__ = clamp(math.sqrt(random_numbers_btw_zero_and_radius_min_spacing[0])*(radius - spacing), 0, radius - spacing)
-    x_coord = radius__*math.cos(theta__) + center_[0]
-    y_coord = radius__*math.sin(theta__) + center_[1]    
-    return np.array([x_coord, y_coord])
+
+def get_random_point(center_, size_=1):
+    random_numbers_btw_zero_and_radius_min_spacing = np.array([-1.0]*size_)
+    random_numbers_btw_zero_and_two_pi = np.array([-1.0]*size_)
+    if current_distribution == 0: # random
+        random_numbers_btw_zero_and_two_pi = np.random.random(size=size_)
+        random_numbers_btw_zero_and_radius_min_spacing = np.random.random(size=size_)
+    elif current_distribution == 1: # gaussian
+        for i in range(size_):
+            while random_numbers_btw_zero_and_radius_min_spacing[i] < 0.0 or random_numbers_btw_zero_and_radius_min_spacing[i] >= 1.0: 
+                random_numbers_btw_zero_and_radius_min_spacing[i] = np.random.normal(loc=0.5, scale=0.2, size=1)[0]
+            while random_numbers_btw_zero_and_two_pi[i] < 0.0 or random_numbers_btw_zero_and_two_pi[i] >= 1.0: 
+                random_numbers_btw_zero_and_two_pi[i] = np.random.uniform(size=1)[0]
+    elif current_distribution == 2: # uniform
+        random_numbers_btw_zero_and_radius_min_spacing = np.random.uniform(size=size_)
+        random_numbers_btw_zero_and_two_pi = np.random.uniform(size=size_)
+    res = []
+    for i in range(size_):
+        random_numbers_btw_zero_and_two_pi[i] = random_numbers_btw_zero_and_two_pi[i]*2*math.pi
+        theta__ = clamp(random_numbers_btw_zero_and_two_pi[i], 0, 2*math.pi)
+        radius__ = 0
+        if current_distribution == 0 or current_distribution == 2:
+            radius__ = clamp(math.sqrt(random_numbers_btw_zero_and_radius_min_spacing[i])*(radius - spacing), 0, radius - spacing)
+        else:
+            radius__ = clamp(random_numbers_btw_zero_and_radius_min_spacing[i]*(radius - spacing), 0, radius - spacing)
+        x_coord = radius__*math.cos(theta__) + center_[0]
+        y_coord = radius__*math.sin(theta__) + center_[1]    
+        res.append(np.array([x_coord, y_coord]))
+    return res
 
 
 def generate_random_points(numPoints):
@@ -48,15 +76,14 @@ def generate_random_points(numPoints):
     center = ((SCREEN_HEIGHT + 250) / 2, SCREEN_HEIGHT / 2)
     global points
     points.clear()
-    for i in range(numPoints):
-        points.append(get_random_point(center))
+    points = get_random_point(center, numPoints)
     center = np.mean(points, axis=0)
 
 
 def step_randomized_incremental(surface):
     global center
     global radius
-    p = get_random_point()
+    p = get_random_point(center)[0]
     points.append(p)
     clock_start = time.time()
     incremental_dt.add_point(p)
@@ -115,7 +142,7 @@ def step_flipping(surface):
     global center
     global radius
     incremental_dt.reset(center, radius * 50)
-    p = get_random_point()
+    p = get_random_point(center)[0]
     points.append(p)
     clock_start = time.time()
     # TODO Fortune's Algorithm impl.
@@ -184,7 +211,10 @@ def event_loop(current_menu):
         screen.blit(NOP_Info, (NOP_RECT.x - 50, NOP_RECT.y - 5))
         screen.blit(NOP_TEXT, (NOP_RECT.x + 5, NOP_RECT.y))
         NOP_RECT.w = max(100, NOP_TEXT.get_width() + 10)
-
+        global current_distribution
+        global distribution_texts
+        DISTRIBUTION = Button(image=None, pos=(1150, 270), text_input=distribution_texts[current_distribution],
+                      font=get_font(25), base_color="Black", hovering_color="Yellow")
         GENERATE = Button(image=None, pos=(1150, 370), text_input="Generate",
                       font=get_font(25), base_color="Black", hovering_color="Yellow")
         STEP = Button(image=None, pos=(1155, 420), text_input="Next Step",
@@ -194,20 +224,26 @@ def event_loop(current_menu):
         BACK = Button(image=None, pos=(1150, 725), text_input="Back to Main Menu",
                       font=get_font(25), base_color="Black", hovering_color="Yellow")
 
-        for button in [GENERATE, STEP, CLEAR, BACK]:
+
+        for button in [DISTRIBUTION, GENERATE, STEP, CLEAR, BACK]:
             if not(current_menu == 1 and button == STEP):
-                button.changeColor(mouse)
-                button.update(screen)
+                button.mouseMoveEvent(mouse)
+                button.draw(screen)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                if BACK.checkForInput(mouse):
+                if BACK.mousePressEvent(mouse):
                     consecutive_nexts = 0
                     main_menu()
-                elif GENERATE.checkForInput(mouse):
+                elif DISTRIBUTION.mousePressEvent(mouse):
+                    current_distribution += 1
+                    current_distribution %= 3
+                    next_text = distribution_texts[current_distribution]
+                    DISTRIBUTION.setText(next_text, mouse)
+                elif GENERATE.mousePressEvent(mouse):
                     if no_of_points == "":
                         no_of_points = "10"
                     no = int(no_of_points)
@@ -219,7 +255,7 @@ def event_loop(current_menu):
                         generate_fortunes(pic, no)
                     elif current_menu == 2:
                         generate_flipping(pic, no)
-                elif STEP.checkForInput(mouse):
+                elif STEP.mousePressEvent(mouse):
                     if consecutive_nexts == 0:
                         clear(pic)
                         no_of_points = ""
@@ -234,7 +270,7 @@ def event_loop(current_menu):
                         step_randomized_incremental(pic)
                     elif current_menu == 2:
                         step_flipping(pic)
-                elif CLEAR.checkForInput(mouse):
+                elif CLEAR.mousePressEvent(mouse):
                     no_of_points = ""
                     consecutive_nexts = 0
                     clear(pic)
@@ -280,21 +316,21 @@ def main_menu():
                             font=get_font(25), base_color="Black", hovering_color="Yellow")
 
         for button in [INC_BUTTON, FORTUNE_BUTTON, FLIP_BUTTON, QUIT]:
-            button.changeColor(mouse)
-            button.update(screen)
+            button.mouseMoveEvent(mouse)
+            button.draw(screen)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if INC_BUTTON.checkForInput(mouse):
+                if INC_BUTTON.mousePressEvent(mouse):
                     event_loop(0)
-                elif FORTUNE_BUTTON.checkForInput(mouse):
+                elif FORTUNE_BUTTON.mousePressEvent(mouse):
                     event_loop(1)
-                elif FLIP_BUTTON.checkForInput(mouse):
+                elif FLIP_BUTTON.mousePressEvent(mouse):
                     event_loop(2)
-                elif QUIT.checkForInput(mouse):
+                elif QUIT.mousePressEvent(mouse):
                     pygame.quit()
                     sys.exit()
                     
